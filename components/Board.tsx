c51a375b4bf9eb079969ab206d50ad3d0a88d747
macielg1 <guflumaciel@gmail.com>
2023-09-20 21:07:09 -0300

    added dnd kit functionality to the main board

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,5 +1,53 @@
 "use client";
+import { ListType } from "@/utils/types";
+import { DndContext, PointerSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
+import { useMemo, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const defaultLists: ListType[] = [
+  {
+    id: uuidv4(),
+    title: "Todo",
+  },
+  {
+    id: uuidv4(),
+    title: "Finished",
+  },
+];
 
 export default function Board() {
-  return <main className="grid pt-10 pb-4 w-full place-items-center overflow-x-auto "></main>;
+  const [lists, setLists] = useState<ListType[]>(defaultLists);
+
+  const pointerSensor = useSensor(PointerSensor, {
+    activationConstraint: {
+      distance: 8,
+    },
+  });
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: {
+      delay: 250,
+      tolerance: 5,
+    },
+  });
+
+  const sensors = useSensors(pointerSensor, touchSensor);
+
+  const listIds = useMemo(() => lists.map((i) => i.id), [lists]);
+
+  return (
+    <main className="grid pt-10 pb-4 w-full place-items-center overflow-x-auto ">
+      <DndContext sensors={sensors} id="board">
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4  ">
+            <SortableContext items={listIds}>
+              {lists.map((list) => (
+                <>{list.title}</>
+              ))}
+            </SortableContext>
+          </div>
+        </div>
+      </DndContext>
+    </main>
+  );
 }
---
+++ utils/types.ts
@@ -0,0 +1,10 @@
+export type ListType = {
+  id: string;
+  title: string;
+};
+
+export type ItemType = {
+  id: string;
+  listId: string;
+  content: string;
+};
