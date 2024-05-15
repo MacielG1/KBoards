9c01b3441dc9a10d4b23fb7d787b011dfd4b6bd0
macielg1 <guflumaciel@gmail.com>
2024-05-15 16:46:41 -0300

    changed the method to substring instead of substr

--- utils/getConstrastColor.ts
+++ utils/getConstrastColor.ts
@@ -1,9 +1,9 @@
 export default function getContrastColor(hexcolor: string) {
   if (!hexcolor) return "#000"; // Default to black if no color provided
   const hex = hexcolor.replace("#", "");
-  const r = parseInt(hex.substr(0, 2), 16);
-  const g = parseInt(hex.substr(2, 2), 16);
-  const b = parseInt(hex.substr(4, 2), 16);
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
   const yiq = (r * 299 + g * 587 + b * 114) / 1000;
   return yiq >= 128 ? "#000" : "#fff"; // Return black or white based on contrast
 }
